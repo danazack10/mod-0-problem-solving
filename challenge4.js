4db8121332b09c038653453b85abc01b78c13392
Dana Zack <danaezack@gmail.com>
2023-09-05 20:17:54 -0600

    Complete challenge 4

--- challenge4.js
+++ challenge4.js
@@ -2,11 +2,19 @@
 
 
 // Overall Goal 
-
+// Print out only the strings within an array that end with "ing"
 
 
 // Pseudocode
-
+// I will need a loop for this so that the code can evaluate each string's ending
+// I will need to google a method that will help code look at the ending specifically
+// Google suggests: .endsWith()
 
 
 // Final Solution
+var hobbies = ["running", "dancing", "bikes", "books", "baking"];
+for (i = 0; i < hobbies.length; i++){
+    if (hobbies[i].endsWith("ing")){
+        console.log(hobbies[i]);
+    }
+}
