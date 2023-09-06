2586572df11174070aec69df1b4a92b855d45b67
Dana Zack <danaezack@gmail.com>
2023-09-05 18:31:18 -0600

    Complete challenge 3

--- challenge3.js
+++ challenge3.js
@@ -4,14 +4,18 @@
 
 
 // Overall Goal 
-
-
+// Write a function that identifies all instances of "s" characters within a string
+// Then remove all instances of "s" characters
+// Finally, print the new string
 
 // Pseudocode
-
-
+var string = "A silly short sentence with several s words"
+console.log(string.replaceAll("s", ""));
 
 // Final solution
-
-
+function removeLetterS(string){
+    var solution = string.replaceAll("s", "").replaceAll("S", "");
+    return solution
+}
+console.log(removeLetterS("So many sassy salamanders slithering..."))
 
