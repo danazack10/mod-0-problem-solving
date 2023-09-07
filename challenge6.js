78251d4dc5e9a66b865b4f35575a25446add772f
Dana Zack <danaezack@gmail.com>
2023-09-07 17:41:49 -0600

    Complete challenge 6

--- challenge6.js
+++ challenge6.js
@@ -1,11 +1,17 @@
-Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized.
-For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
+// Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized.
+// For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
 
 
-Overall Goal 
+// Overall Goal 
+// Change a string so that the first letter of every word is capitalized. 
 
+// Pseudocode
+// The method .split() should divide a string variable into an array of individual words
 
-Pseudocode
-
-
-Final Solution
+// Final Solution
+var sentence = "Turing is the best."
+var sentenceArray = sentence.split(" ");
+for (i = 0; i < sentenceArray.length; i++){
+    sentenceArray[i].charAt(0).toLowerCase() + sentenceArray[i].slice(1);
+}
+console.log(sentenceArray.join(" "));
